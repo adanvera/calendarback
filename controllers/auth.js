1bc75ebd7ca9ae5f57c09c51d22fecb8279c59a2
adanvera <gustavoadanvera@gmail.com>
2024-10-10 17:21:58 +0200

    agrega name lastname y uid en token renew

--- controllers/auth.js
+++ controllers/auth.js
@@ -122,6 +122,9 @@ const renewToken = async (req, res = response) => {
         const token = await generarJWT(uid, name);
         res.json({
             ok: true,
+            uid,
+            name,
+            lastname,
             token
         });
     } catch (error) {
